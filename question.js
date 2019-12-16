a6164a9572eef0aff1768a567418c0aaf4601072
Ben Abbott <babbott85@gmail.com>
2019-12-16 16:46:56 -0700

    all questions added to questions.js file

---
+++ question.js
@@ -0,0 +1,30 @@
+{
+    title = "Commonly used data types DO NOT include:",
+        choices = ["strings", "booleans", "alerts", "numbers"],
+            answer= "alerts"
+}
+
+{
+    title: "The condition in an if / else statement is enclosed within ____.",
+        choices = ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer=  "parentheses"
+}
+
+{
+    title: "Which of the following is not a vanilla javascript command."
+        choices = ["document.getElementById", "document.queryselectorAll", ".on", ".setAttribute"]
+            answer= ".on"
+}
+
+{
+    title: "How does every for each loop start?"
+        choices = ["condition", "array name", "if statement", "function call"]
+            answer= "array name"
+}
+
+{
+    title: "What is the command to add JS commands to an HTML document?"
+        choices = [".appendChild", ".concat", "JSON.stringify", ".split"]
+            answer= ".appendChild"
+}
+
