dbc6761604cfc6f54964088702aa74a6983c8610
Ben Abbott <babbott85@gmail.com>
2019-12-16 17:22:01 -0700

    a bit of styling done and start button added

---
+++ script.js
@@ -0,0 +1,13 @@
+var startEl= document.querySelector("#start-button");
+
+
+
+
+
+startEl.addEventListener("click", function(event){
+  event.preventDefault();
+if (event.target.matches(button)) {
+    
+}
+
+})
