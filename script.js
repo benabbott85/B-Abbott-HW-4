4ea10ba859cc01e32c2584eeed7d2e5d5ab1b522
Ben Abbott <babbott85@gmail.com>
2019-12-18 09:59:04 -0700

    questions now circle thru after starting game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var startEl= document.getElementById("start-button");
 var highScore = document.getElementById("highScore");
 var timerEL= document.getElementById("timer");
-var qIndex = 0
+var qIndex = 0;
 var questionsEl = document.getElementById("questions");
 var choicesEl = document.getElementById("choices");
 
@@ -9,28 +9,42 @@ var choicesEl = document.getElementById("choices");
 
 function startQuiz(){
     var getStart = document.getElementById("start");
-    getStart.setAttribute("class", "hide")
-    questionsEl.removeAttribute("class")
+    getStart.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
     // quizTimer();
-    question();
+    displayQuestion();
+    // console.log("quizstarted")
 }
 
 
-function question(){
+function displayQuestion(){
     var currentQuestion = questions[qIndex]
-    questionsEl.textContent = currentQuestion.title
+
+    var titleEL = document.getElementById("qTitle")
+    titleEL.textContent = currentQuestion.title;
     choicesEl.innerHTML = "";
     
     currentQuestion.choices.forEach(function(choice,i){
+
         var choiceBtn=document.createElement("button")
         choiceBtn.setAttribute("class", "choice")
         choiceBtn.setAttribute("value", choice)
-        choiceBtn.textContent = i + 1 + ". " + choice
-        // choiceBtn.onclick = 
-        choicesEl.appendChild(choiceBtn)
-    })
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick = click;
+        choicesEl.appendChild(choiceBtn);
+        
+        
+    }) 
 }
 
+function click (){
+    qIndex++;
+    if (qIndex === questions.length){
+        console.log("gameover")
+    } else {
+        displayQuestion();
+    }
+}
 
 function quizTimer (){
     var quizCountdown = 60;
@@ -48,7 +62,15 @@ function quizTimer (){
                     
                        
 }
-startEl.onclick = startQuiz
+
+// function choiceQ (){
+//     for (var i = 0; i < questions(i).choices(i); i++)
+    
+// }
+
+
+startEl.onclick = startQuiz;
+choicesEl.onclick = choices
 // startEl.addEventListener("click", function(){
 //     console.log("work please")
 // })
