b44dc2f419d75bb60e39515030feb28ee4f5853d
Ben Abbott <babbott85@gmail.com>
2019-12-18 18:35:45 -0700

    functionality is all wired up

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-Technology used:
+## Technology used:
 - HTML
 - CSS
 - JavaScript
 
-Deployed version:
+## Deployed version:
 https://benabbott85.github.io/B-Abbott-HW-4/
 
+## About
+- When the page opens it will be laid out with an image on the center of the page and the button to start the game in the top left of the page
+- When you click the start button, it will start the timer and begin to cycle through the 5 questions that are a part of the quiz.
+- The sco
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var startEl= document.getElementById("start-button");
+var startEl = document.getElementById("start-button");
 var highScore = document.getElementById("highScore");
-var timerEL= document.getElementById("timer");
+var timerEL = document.getElementById("timer");
 var qIndex = 0;
 var questionsEl = document.getElementById("questions");
 var choicesEl = document.getElementById("choices");
 // var austinEL = document.getElementsById("austin");
+var score = 0;
+var storedHighScore = 0;
+var quizCountdown = 60;
+var currentQuestion;
+// var currentQuestion;
 
 
-function startQuiz(){
+function startQuiz() {
+    $("#austin").empty();
     var getStart = document.getElementById("start");
 
     getStart.setAttribute("class", "hide");
@@ -18,61 +24,97 @@ function startQuiz(){
 }
 
 
-function displayQuestion(){
-    var currentQuestion = questions[qIndex]
+function displayQuestion() {
+    currentQuestion = questions[qIndex]
 
     var titleEL = document.getElementById("qTitle")
     titleEL.textContent = currentQuestion.title;
     choicesEl.innerHTML = "";
-    
-    currentQuestion.choices.forEach(function(choice,i){
 
-        var choiceBtn=document.createElement("button")
+    currentQuestion.choices.forEach(function (choice, i) {
+
+        var choiceBtn = document.createElement("button")
         choiceBtn.setAttribute("class", "choice")
         choiceBtn.setAttribute("value", choice)
         choiceBtn.textContent = i + 1 + ". " + choice;
         choiceBtn.onclick = click;
         choicesEl.appendChild(choiceBtn);
-        
-        
-    }) 
+
+
+
+    })
+    console.log(qIndex)
+
 }
 
-function click (){
+function click() {
     qIndex++;
-    if (qIndex === questions.length){
+    if (($(this).attr("value")) !== currentQuestion.answer) {
+        quizCountdown = quizCountdown - 5;
+
+    } else {
+        score += quizCountdown;
+        console.log("score" + score);
+    }
+    if (qIndex === questions.length) {
         console.log("gameover")
+        $("#vanish").empty();
+        $("#vanish").text("Your score is: " + score)
     } else {
         displayQuestion();
     }
 }
 
-function quizTimer (){
-    var quizCountdown = 60;
-    var timerInterval = setInterval(function(){
+function quizTimer() {
+    // var quizCountdown = 60;
+    var timerInterval = setInterval(function () {
         timerEL.textContent = "time " + quizCountdown;
         quizCountdown--;
-        
-        if(quizCountdown === 0 || currentQuestion === qIndex.length){
+        console.log("countdown")
+        if (quizCountdown === 0 || questions.length === qIndex) {
             clearInterval(timerInterval);
             timerEL.textContent = "";
-            questions.textContent = "Score: " + highScore;
-            
-        }
+            if (score > storedHighScore) {
+                $("#vanish").text("New High Score! " + score);
+                $(".initial").show();
+                console.log("I win")
+            } else {
+                $("#vanish").text("Score: " + score);
+                console.log("I lose")
 
-    }, 1000)
-                    
-                       
+            }
+        }
+        }, 100)
+
+
 }
 
 // function choiceQ (){
 //     for (var i = 0; i < questions(i).choices(i); i++)
-    
+
 // }
 
 
 startEl.onclick = startQuiz;
 choicesEl.onclick = choices;
+
+$(".initial").on("click", "button", function(){
+    storedHighScore = score;
+    var initials = $("#init").val();
+    console.log(initials)
+    $(".initial").empty();
+    var highScoreAry = [initials, storedHighScore];
+    var highScore = highScoreAry.toString();
+    localStorage.setItem("High Score", highScore);
+})
+
+var sto = localStorage.getItem("High Score");
+console.log(sto);
+sto= sto.split(" , ");
+console.log(sto);
+storedHighScore = sto[1];
+
+
 
 
 
